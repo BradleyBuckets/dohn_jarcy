6c42861837532768514045ae8b4fa11ca57d9982
Bradley Griffin <bigriff11@gmail.com>
2023-04-13 13:26:19 -0600

    Change some colors and try to add an img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className="App">
+    <div className=" h-screen w-screen bg-zinc-800">
       <div className=" text-2xl">Content</div>
+      <img src="src/assets/dohn.png" alt="Greyscale business man" />
+      <img
+        src="/Users/bradleygriffin/repos/dohn_jarcy/src/assets/logo512.png"
+        alt="react logo"
+      />
     </div>
   );
 }
